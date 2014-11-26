e297bdd66fbd3dbb95dfa4df3ed79cd7da307f77
ssseeebbb <sebastien.goldberg@hotmail.com>
2014-11-26 09:11:05 +0100

    Changement Php git tuto

--- send.php
+++ send.php
@@ -1,76 +1,95 @@
 <?php
+	{  
+  	"require": {
+    "sendgrid/sendgrid": "2.1.1"
+  		}
+	}
 
-	echo 'coucou';
-    // CONDITIONS NOM
-   if ( (isset($_POST['nom'])) && (strlen(trim($_POST['nom'])) > 0) ):
-        $nom = stripslashes(strip_tags($_POST['nom']));
-    else:
-        echo "Merci d'écrire un nom <br />";
-        $nom = '';
-    endif;
+	// initialize the SendGrid
+	$sendgrid = new SendGrid('ssseeebbb', 'Sebastien007');
 
-    // CONDITIONS SUJET
-    if ( (isset($_POST['subject'])) && (strlen(trim($_POST['subject'])) > 0) ):
-        $sujet = stripslashes(strip_tags($_POST['subject']));
-    else:
-        echo "Merci d'écrire un sujet <br />";
-        $sujet = '';
-    endif;
+	//Create a new SendGrid Email object and add your message details.
+	$email = new SendGrid\Email();
+	$email->addTo('sebastien.goldberg@hotmail.com')->
+       addTo('info@nexthappyhours.com')->
+       setFrom($nom)->
+       setSubject($sujet)->
+       setText($message)->
+       //setHtml('<strong>Hello World!</strong>');
 
-    // CONDITIONS EMAIL
-    if ( (isset($_POST['mail'])) && (strlen(trim($_POST['mail'])) > 0) && (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) ):
-        $email = stripslashes(strip_tags($_POST['mail']));
-    elseif (empty($_POST['mail'])):
-        echo "Merci d'écrire une adresse email <br />";
-        $email = '';
-    else:
-        echo 'Email invalide :(<br />';
-        $email = '';
-    endif;
-
-    // CONDITIONS MESSAGE
-    if ( (isset($_POST['message'])) && (strlen(trim($_POST['message'])) > 0) ):
-        $message = stripslashes(strip_tags($_POST['message']));
-    else:
-        echo "Merci d'écrire un message<br />";
-        $message = '';
-    endif;
+    $sendgrid->send($email);
 
 
-    $objet        = "Springbok -" . $sujet;
- 	$contenu      = "Nom de l'expéditeur : " . $nom . "\r\n";
-    $contenu      = $message."\r\n\n";
+ //    // CONDITIONS NOM
+ //   if ( (isset($_POST['nom'])) && (strlen(trim($_POST['nom'])) > 0) ):
+ //        $nom = stripslashes(strip_tags($_POST['nom']));
+ //    else:
+ //        echo "Merci d'écrire un nom <br />";
+ //        $nom = '';
+ //    endif;
 
- $url = 'https://api.sendgrid.com/';
- $user = 'ssseeebbb';
- $pass = 'Sebastien007.'; 
+ //    // CONDITIONS SUJET
+ //    if ( (isset($_POST['subject'])) && (strlen(trim($_POST['subject'])) > 0) ):
+ //        $sujet = stripslashes(strip_tags($_POST['subject']));
+ //    else:
+ //        echo "Merci d'écrire un sujet <br />";
+ //        $sujet = '';
+ //    endif;
 
- $params = array(
-      'api_user' => $user,
-      'api_key' => $pass,
-      'to' => 'sebastien.goldberg@hotmail.com	',
-      'subject' => $objet,
-      'html' => 'testing body',
-      'text' => $contenu,
-      'from' => $email,
-   );
- $request = $url.'api/mail.send.json';
- // Generate curl request
- $session = curl_init($request);
- // Tell curl to use HTTP POST
- curl_setopt ($session, CURLOPT_POST, true);
+ //    // CONDITIONS EMAIL
+ //    if ( (isset($_POST['mail'])) && (strlen(trim($_POST['mail'])) > 0) && (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) ):
+ //        $email = stripslashes(strip_tags($_POST['mail']));
+ //    elseif (empty($_POST['mail'])):
+ //        echo "Merci d'écrire une adresse email <br />";
+ //        $email = '';
+ //    else:
+ //        echo 'Email invalide :(<br />';
+ //        $email = '';
+ //    endif;
 
- // Tell curl that this is the body of the POST
- curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+ //    // CONDITIONS MESSAGE
+ //    if ( (isset($_POST['message'])) && (strlen(trim($_POST['message'])) > 0) ):
+ //        $message = stripslashes(strip_tags($_POST['message']));
+ //    else:
+ //        echo "Merci d'écrire un message<br />";
+ //        $message = '';
+ //    endif;
 
- // Tell curl not to return headers, but do return the response
- curl_setopt($session, CURLOPT_HEADER, false);
+
+ //    $objet        = "Springbok -" . $sujet;
+ // 	$contenu      = "Nom de l'expéditeur : " . $nom . "\r\n";
+ //    $contenu      = $message."\r\n\n";
+
+ // $url = 'https://api.sendgrid.com/';
+ // $user = 'ssseeebbb';
+ // $pass = 'Sebastien007.'; 
+
+ // $params = array(
+ //      'api_user' => $user,
+ //      'api_key' => $pass,
+ //      'to' => 'sebastien.goldberg@hotmail.com	',
+ //      'subject' => $objet,
+ //      'html' => 'testing body',
+ //      'text' => $contenu,
+ //      'from' => $email,
+ //   );
+ // $request = $url.'api/mail.send.json';
+ // // Generate curl request
+ // $session = curl_init($request);
+ // // Tell curl to use HTTP POST
+ // curl_setopt ($session, CURLOPT_POST, true);
+
+ // // Tell curl that this is the body of the POST
+ // curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // // Tell curl not to return headers, but do return the response
+ // curl_setopt($session, CURLOPT_HEADER, false);
  
 
- curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
- // obtain response
- $response = curl_exec($session);
- curl_close($session);
- // print everything out
+ // curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+ // // obtain response
+ // $response = curl_exec($session);
+ // curl_close($session);
+ // // print everything out
 
 ?>
